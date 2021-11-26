4c57840efdecc2fd25bd253ef06f76318a869ce3
Daisy Lin <daisy.lin@access-company.com>
2021-11-26 10:51:37 +0800

    GTM text

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,7 +1,7 @@
 <template v-for="col in tabledate">
   <div id="date-picker">
 
-    <p>{{ now }}</p>
+    <p>{{ now }}+8 Asia/Taipei </p>
 
     <table>
       <tr>
@@ -29,38 +29,56 @@ export default {
   data(){
     return{
       now: null,
-      tableData: {}
+      tableData: {},
+      inputdate: null,
+      getDate: null
     }
   },
   methods: {
     getURL: function(){
       dayjs.locale('zh-tw')
-      this.now = dayjs()
+      this.now = dayjs().add(8, 'h')
     },
-    getDate: function(){
+    getPeriod: function(){
       // https://programmer-note.hatenablog.com/entry/2021/09/29/182707
       this.dayjs = inject('dayjs')
       var customParseFormat = require('dayjs/plugin/customParseFormat')
       dayjs.extend(customParseFormat)
 
+      // this.inputdate = this.$route.query.startTime;
+      
+      // 優先順序:
+      // 1. get URL
+      // 2. get localStorage
+      // 3. get today
+      if(this.$route.query.startTime){
+        this.inputdate = this.$route.query.startTime;
+        localStorage.datePickerDate = this.$route.query.startTime;
+        this.getDate = this.inputdate;
+      }else if (localStorage.datePickerDate){
+        this.getDate = localStorage.datePickerDate
+      }else{
+        this.getDate = this.now.day(1).format('YYYY年M月DD日 (一)')
+      }
+
       var tableData = [{
-        "start": dayjs(this.$route.query.startTime).day(1).format('YYYY年M月DD日 (一)'),
-        "end"  : dayjs(this.$route.query.startTime).day(1).add(13, 'd').format('YYYY年M月DD日 (日)')
+        "start": dayjs(this.getDate).day(1).format('YYYY年M月DD日 (一)'),
+        "end"  : dayjs(this.getDate).day(1).add(13, 'd').format('YYYY年M月DD日 (日)')
       }];
 
       for (var i=1; i<12; i++){
         var period = 13*i;
         tableData.push({
-          "start": dayjs(this.$route.query.startTime).day(1).add(period+(1*i), 'd').format('YYYY年M月DD日 (一)'), 
-          "end"  : dayjs(this.$route.query.startTime).day(1).add(period+1*13+i, 'd').format('YYYY年M月DD日 (日)')
+          "start": dayjs(this.getDate).day(1).add(period+(1*i), 'd').format('YYYY年M月DD日 (一)'), 
+          "end"  : dayjs(this.getDate).day(1).add(period+1*13+i, 'd').format('YYYY年M月DD日 (日)')
         })
       }
       this.tableDataStart = tableData
-    }
+    },
   },
   mounted(){
     this.getURL();
-    this.getDate();
+    this.getPeriod();
   },
 }
 </script>
@@ -70,7 +88,7 @@ export default {
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
-  width: 70%;
+  width: 80%;
   margin: 0 auto;
 }
 
@@ -83,4 +101,5 @@ td, th {
 tr:nth-child(even) {
   background-color: #dddddd;
 }
+
 </style>
